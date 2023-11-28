42b6b8e1f7cd242f5d632fd2ebef8d70291ca4a7
rrose22 <144692689+rrose22@users.noreply.github.com>
2023-11-28 14:17:07 -0500

    project created

---
+++ restorante/components/ButtonBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const ButtonBar = ({ buttons, onPress }) => {
+  return (
+    <View style={styles.container}>
+      {buttons.map((button, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.button}
+          onPress={() => onPress(index)}
+        >
+          <Text style={styles.buttonText}>{button}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: '#3498db',
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default ButtonBar;
